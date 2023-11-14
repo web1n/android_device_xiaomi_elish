e79e25a2da77d8a9a9823060e21f6271c3281e4f
LuK1337 <priv.luk@gmail.com>
2023-11-14 20:17:10 +0100

    elish: extract-files: Prevent adding duplicated dependencies
    Change-Id: I186c38583670d509d4e0a0c89bc682706036f1ea

--- extract-files.sh
+++ extract-files.sh
@@ -22,7 +22,7 @@ EOF
             ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib/hw/audio.primary.elish.so)
             [ "$2" = "" ] && return 0
