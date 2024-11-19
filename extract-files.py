e67faece4fe298c5466364da6ef5ddf14597aec9
LuK1337 <priv.luk@gmail.com>
2024-11-19 12:38:18 +0100

    elish: Enable elfchecks for libgrallocutils
    Reference: I11dec853d8cfaddb01ba96a2e76be38fe2d52073
    Change-Id: I58f44f8486efff8ba26fe36860d890412f09316d

--- extract-files.py
+++ extract-files.py
@@ -9,9 +9,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
-    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -32,15 +30,9 @@ blob_fixups: blob_fixups_user_type = {
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
 }  # fmt: skip
 
-lib_fixups: lib_fixups_user_type = {
-    **lib_fixups,
-    (
-        'libgrallocutils',
-    ): lib_fixup_remove,
-}
-
 namespace_imports = [
     'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/qcom-caf/sm8250',
     'hardware/xiaomi',
     'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm8250-common',
