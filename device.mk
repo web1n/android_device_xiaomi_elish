7359d7fff44e7789cfb1104dd9e54efe51e14c32
Bin <9633157+web1n@users.noreply.github.com>
2025-02-15 12:11:18 +0800

    elish: Move to Xiaomi fingerprint AIDL

--- device.mk
+++ device.mk
@@ -37,7 +37,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
     vendor.xiaomi.hardware.fx.tunnel@1.0.vendor
 
 PRODUCT_COPY_FILES += \
