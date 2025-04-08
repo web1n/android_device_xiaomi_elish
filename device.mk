99d8a7894d3f97ae6154a59f184e347986d9fc0e
Bin <9633157+web1n@users.noreply.github.com>
2025-04-08 16:13:57 +0800

    Revert "elish: build ForceRotation"
    This reverts commit f94a608797c1f8aa8b593677beebc754bdb97ade.

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# ForceRotation
-PRODUCT_PACKAGES += \
-    ForceRotation
-
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1.vendor
