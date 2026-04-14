7635d7a6496f3077c7a91ccdfa607462c9e540ab
web1n <9633157+web1n@users.noreply.github.com>
2026-04-14 10:26:42 +0800

    Fix KeyHandler to use context for InputManager retrieval

--- parts/src/org/lineageos/pad_parts/button/KeyHandler.java
+++ parts/src/org/lineageos/pad_parts/button/KeyHandler.java
@@ -43,7 +43,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
     public KeyHandler(Context context) {
         mContentResolver = context.getContentResolver();
-        mInputManager = InputManager.getInstance();
+        mInputManager = context.getSystemService(InputManager.class);
 
         // Enable button settings activity
         ButtonUtils.enableButtonSettingsActivity(context);
